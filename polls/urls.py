a17f11bc1811c89631faf3c0ebd672d8a186acd1
Mobin Pourabedini <mobin.pourabedini@divar.ir>
2024-05-12 14:14:06 +0330

    add simple entry endpoint

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("entry_endpoint/", views.entry_endpoint, name="entry_endpoint"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.utils import json
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+@api_view(['GET'])
+def entry_endpoint(request):
+    # Retrieve query parameters
+    app_slug = request.GET.get('app_slug', '')
+    post_token = request.GET.get('post_token', '')
+    return_url = request.GET.get('return_url', '')
+    #
+    # # Process the query parameters and perform necessary actions
+    data = {
+        'app_slug': app_slug,
+        'post_token': post_token,
+        'return_url': return_url,
+    }
+    return HttpResponse(json.dumps(data), content_type='application/json')
