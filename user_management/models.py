11660149e8fd2785fff076283a35d55a512e20b2
Mobin Pourabedini <mobin.pourabedini@divar.ir>
2024-05-14 12:42:24 +0330

    add post model and get access key for the specific post

--- user_management/models.py
+++ user_management/models.py
@@ -5,3 +5,4 @@ class Post (models.Model):
     token = models.CharField(max_length=50, primary_key=True)
     code = models.CharField(max_length=255)
     user = models.CharField(max_length=50, null=True)
+    access_token = models.CharField(max_length=500, null=True)
