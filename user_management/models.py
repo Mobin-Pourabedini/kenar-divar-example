b404881998473544c3d2905040976a9036ebfd34
Alireza Samimi <alireza.samimi@divar.ir>
2024-05-15 18:12:39 +0330

    hahahaa

--- user_management/models.py
+++ user_management/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 
-class Post (models.Model):
-    token = models.CharField(max_length=50, primary_key=True)
-    code = models.CharField(max_length=255)
-    user = models.ForeignKey('user_management.User', max_length=50, null=True, on_delete=models.SET_NULL)
-    access_token = models.CharField(max_length=500, null=True)
-
-
-class User (models.Model):
+class User(models.Model):
     phone = models.CharField(max_length=50, primary_key=True)
     access_token = models.CharField(max_length=500)
+
+
+class Post(models.Model):
+    token = models.CharField(max_length=50, primary_key=True)
+    code = models.CharField(max_length=255)
+    user = models.ForeignKey(User, max_length=50, null=True, on_delete=models.SET_NULL)
+    access_token = models.CharField(max_length=500, null=True)
