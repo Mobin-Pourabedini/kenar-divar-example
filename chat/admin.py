6e8ce1013c1a917a2665d7aae9cc114ee2669ff1
Mobin Pourabedini <mobin.pourabedini@divar.ir>
2024-06-05 01:00:07 +0330

    Remove: unused django model

--- chat/admin.py
+++ chat/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 
-from chat.models import ChatSession, ChatMessage
+from chat.models import ChatSession
 
 
 @admin.register(ChatSession)
 class ChatAdmin(admin.ModelAdmin):
     list_display = ('id', 'user_id', 'peer_id', 'supplier_id', 'demand_id', 'created_at', 'updated_at')
-
-
-@admin.register(ChatMessage)
-class ChatMessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user_id', 'peer_id', 'message', 'created_at')
